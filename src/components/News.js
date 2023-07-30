b14b03ab0f1887aa8eb5b66a02c9ab8244fb612d
KaustubhDalal <kaustubhvd5@gmail.com>
2023-07-31 00:06:23 +0530

    Added API integration changes

---
+++ src/components/News.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
+
+export class News extends Component {
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=373261279e494826a0becdd5783e7cff";
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      articles: parseData.articles,
+    });
+  }
+  render() {
+    return (
+      <div className="container my-3">
+        <h2>News-Vantage Top Headlines</h2>
+        <div className="row">
+          {this.state.articles.map((element) => {
+            return (
+              <div key={element.url} className="col-md-4">
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imgUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default News;
